1bcc01f522faf81f65e6fba3446ff44d7263fa30
anton5267 <antonlistva47@gmail.com>
2026-03-05 01:57:20 +0100

    Improve bot move selection with capture prioritization

--- src/Tests/Chess.Web.IntegrationTests/BotMoveSelectorTests.cs
+++ src/Tests/Chess.Web.IntegrationTests/BotMoveSelectorTests.cs
@@ -55,4 +55,69 @@ public class BotMoveSelectorTests
             move.Source == selectedMove.Source &&
             move.Target == selectedMove.Target);
     }
+
+    [Fact]
+    public void RandomLegalMoveSelector_ShouldPreferHighestValueCapture()
+    {
+        var game = CreateEmptyGame(movingColor: Color.White);
+        var board = game.ChessBoard;
+
+        board.GetSquareByName("a1").Piece = Factory.GetKing(Color.White);
+        board.GetSquareByName("h8").Piece = Factory.GetKing(Color.Black);
+        board.GetSquareByName("d4").Piece = Factory.GetQueen(Color.White);
+        board.GetSquareByName("d5").Piece = Factory.GetPawn(Color.Black);
+        board.GetSquareByName("g4").Piece = Factory.GetRook(Color.Black);
+        board.CalculateAttackedSquares();
+
+        var selector = new RandomLegalMoveSelector();
+        for (var i = 0; i < 20; i++)
+        {
+            selector.TrySelectMove(game, out var selectedMove).Should().BeTrue();
+            selectedMove.Should().NotBeNull();
+            selectedMove.Source.Should().Be("d4");
+            selectedMove.Target.Should().Be("g4");
+            selectedMove.IsCapture.Should().BeTrue();
+        }
+    }
+
+    private static Game CreateEmptyGame(Color movingColor)
+    {
+        var services = new ServiceCollection()
+            .AddTransient<INotificationService, NotificationService>()
+            .AddTransient<ICheckService, CheckService>()
+            .AddTransient<IDrawService, DrawService>()
+            .AddTransient<IUtilityService, UtilityService>()
+            .BuildServiceProvider();
+
+        var white = new Player("white", "white-conn", "white-id")
+        {
+            Color = Color.White,
+            HasToMove = movingColor == Color.White,
+        };
+
+        var black = new Player("black", "black-conn", "black-id")
+        {
+            Color = Color.Black,
+            HasToMove = movingColor == Color.Black,
+        };
+
+        var game = new Game(
+            white,
+            black,
+            services.GetRequiredService<INotificationService>(),
+            services.GetRequiredService<ICheckService>(),
+            services.GetRequiredService<IDrawService>(),
+            services.GetRequiredService<IUtilityService>(),
+            services);
+
+        foreach (var square in game.ChessBoard.Matrix.SelectMany(x => x))
+        {
+            square.Piece = null;
+        }
+
+        game.ChessBoard.CalculateAttackedSquares();
+        game.GameOver = GameOver.None;
+        game.Turn = 1;
+        return game;
+    }
 }
--- src/Web/Chess.Web/Hubs/Bot/RandomLegalMoveSelector.cs
+++ src/Web/Chess.Web/Hubs/Bot/RandomLegalMoveSelector.cs
@@ -16,8 +16,36 @@ namespace Chess.Web.Hubs.Bot
                 return false;
             }
 
+            var captureMoves = legalMoves
+                .Where(x => x.IsCapture)
+                .ToArray();
+
+            if (captureMoves.Length > 0)
+            {
+                var bestCaptureScore = captureMoves
+                    .Max(x => this.GetCaptureScore(game, x));
+                var bestCaptures = captureMoves
+                    .Where(x => this.GetCaptureScore(game, x) == bestCaptureScore)
+                    .ToArray();
+
+                move = bestCaptures[Random.Shared.Next(bestCaptures.Length)];
+                return true;
+            }
+
             move = legalMoves[Random.Shared.Next(legalMoves.Length)];
             return true;
+        }
+
+        private int GetCaptureScore(Game game, LegalMove move)
+        {
+            var targetSquare = game.ChessBoard.GetSquareByName(move.Target);
+            if (targetSquare?.Piece != null)
+            {
+                return targetSquare.Piece.Points;
+            }
+
+            // En passant target squares are empty even though the move is a capture.
+            return 1;
         }
     }
 }
